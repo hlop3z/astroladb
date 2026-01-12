386609a8a3b29a54accf61de0823e658f6275228
hlop3z <23062270+hlop3z@users.noreply.github.com>
2026-01-12 00:34:44 -0600

    Delete user.js

--- cmd/alab/schemas/test/user.js
+++
@@ -1,9 +0,0 @@
-// schemas/test/user.js
-export default table({
-  // id: col.id(),
-  // name: col.string(100),
-  // email: col.email().unique(),
-  // is_active: col.flag(true),
-  // status: col.enum(["active", "inactive"]).default("active"),
-  // author: col.belongs_to("test.user"),
-}).timestamps();
