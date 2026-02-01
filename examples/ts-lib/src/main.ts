96c37345756962b2d6489a07384a0791d2465078
hlop3z <23062270+hlop3z@users.noreply.github.com>
2026-02-01 06:50:07 -0600

    docs: update ts-lib example

--- examples/ts-lib/src/main.ts
+++ examples/ts-lib/src/main.ts
@@ -4,14 +4,11 @@
 import { buildModels, buildRouter, buildMain } from "./templates";
 
 export default gen((schema) => {
-  const api = schema.openapi;
-  const endpoint = api.paths["/schemas"].get.responses["200"]
-    .content["application/json"].example;
-  const namespaces = Object.keys(endpoint.models);
+  const namespaces = Object.keys(schema.models);
   const files: Record<string, string> = {};
 
   for (const ns of namespaces) {
-    const tables = endpoint.models[ns];
+    const tables = schema.models[ns];
     files[ns + "/models.py"] = buildModels(tables);
     files[ns + "/router.py"] = buildRouter(ns, tables);
     files[ns + "/__init__.py"] = 'from .router import router  # noqa: F401\n';
